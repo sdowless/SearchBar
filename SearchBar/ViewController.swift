2052d2ba77d565ca59fca1fc51b1500c99011aef
Stephan Dowless <dowless.stephan@gmail.com>
2019-06-25 21:33:14 -0400

    Initial Commit

---
+++ SearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBar
+//
+//  Created by Stephen Dowless on 6/25/19.
+//  Copyright © 2019 Stephan Dowless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
